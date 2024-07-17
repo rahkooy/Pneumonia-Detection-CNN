e7f538df7cb8ba5e97bf426d6a23c25e2b25df32
PinkPanther123 <h_r_math1123@yahoo.com>
2024-07-17 10:35:25 +0100

    readme and main updated and clened, gitignore added, type hinting added to helper.py

--- README.md
+++ README.md
@@ -4,7 +4,11 @@
 Detecting Pneumonia from X-Ray images using Convolutional Neural Networks
 
 ## Dataset
-[Labeled Chest X-Ray Images](https://www.kaggle.com/datasets/tolgadincer/labeled-chest-xray-images)
+The dataset contains 5,856 Chest X-Ray images. The images are split into a training set (train directory) and a testing set (test directory) of independent patients. There are two classes of images with labels NORMAL and PNEUMONIA. The dataset  is publicly available at 
+[Labeled Chest X-Ray Images](https://www.kaggle.com/datasets/tolgadincer/labeled-chest-xray-images),
+which is itself taken from https://data.mendeley.com/datasets/rscbjbr9sj/3.
+under Licence: CC BY 4.0, and the main paper is https://www.cell.com/cell/fulltext/S0092-8674(18)30154-5
+
 
 ## Requirements
 - `numpy`
@@ -18,9 +22,7 @@ Detecting Pneumonia from X-Ray images using Convolutional Neural Networks
   - Metrics: `confusion_matrix`, `accuracy_score`
 
 ## Utils Directory
-The `utils` directory includes Python files, each of which contains a CNN model and possibly some variants of it. 
-
-### helper.py includes the following auxiliary functions:
+The `utils` directory includes Python files, each of which contains a CNN model and possibly some variants of it. It also contains helper.py, which includes the following auxiliary functions:
 - `datagen_train_dir`
 - `datagen_test_dir`
 - `plot_history`
@@ -28,19 +30,24 @@ The `utils` directory includes Python files, each of which contains a CNN model 
 - `plot_roc_au`
 
 ## Models Considered and Their Performance
-1. **LeNet**: 
+1. **Our Model**: 
+   - Has only seven layers, including two concolutional layers and two maxpooling layers with only ~300,000 parameters
+   - Shows the best accuracy and loss, the same as ResNet with 130 million parameters and other large pre-trained models
+   - 95% accuracy
+
+2. **LeNet**: 
    - Quite old but produces satisfiable results
    - 91% accuracy
    
-2. **AlexNet**: 
+3. **AlexNet**: 
    - Also quite old but produces satisfiable results
    - 91% accuracy
 
-3. **InceptionV3**: 
+4. **InceptionV3**: 
    - Better results than the previous models
    - 94% accuracy
 
-4. **ResNet** with the following variants:
+5. **ResNet** with the following variants:
    - **4.1 With One Fully Connected Layer**:
      - Best possible result among all models
      - 95% accuracy
@@ -54,10 +61,5 @@ The `utils` directory includes Python files, each of which contains a CNN model 
      - Makes the gap between data accuracy/loss and validation accuracy/loss much less
      - 86% accuracy
 
-5. **DenseNet**: 
-   - 95% accuracy
-
-6. **New Model**: 
-   - Has very few layers of normalisation and dropout
-   - Shows similar accuracy and loss as the best pre-trained models
+6. **DenseNet**: 
    - 95% accuracy
--- utils/helper.py
+++ utils/helper.py
@@ -5,6 +5,8 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from sklearn.metrics import confusion_matrix, classification_report, accuracy_score, roc_curve, auc, ConfusionMatrixDisplay
+from keras.preprocessing.image import DirectoryIterator
+from typing import Tuple, List, Dict, Any
 
 # Data generators via ImageDataGenerator
 datagen_train = ImageDataGenerator(
@@ -19,10 +21,10 @@ datagen_test = ImageDataGenerator(
 )
 
 # Train Data Generator
-def datagen_train_dir(dir, in_shape):
+def datagen_train_dir(dir: str, in_shape: Tuple[int, int]) -> DirectoryIterator:
     generator = datagen_train.flow_from_directory(
         dir,
-        target_size=(in_shape[0], in_shape[1]),  # target_size=(224, 224),
+        target_size=(in_shape[0], in_shape[1]),  
         batch_size=32,
         class_mode='binary',  # Since class_mode is binary, labels will be 0 or 1
         classes=['NORMAL', 'PNEUMONIA']  # Specify the class labels explicitly
@@ -30,10 +32,10 @@ def datagen_train_dir(dir, in_shape):
     return generator
 
 # Test Data Generator
-def datagen_test_dir(dir, in_shape):
+def datagen_test_dir(dir: str, in_shape: Tuple[int, int]) -> DirectoryIterator:
     generator = datagen_test.flow_from_directory(
         dir,
-        target_size=(in_shape[0], in_shape[1]),  # target_size=(224, 224),
+        target_size=(in_shape[0], in_shape[1]),  
         batch_size=32,
         class_mode='binary',  # Since class_mode is binary, labels will be 0 or 1
         classes=['NORMAL', 'PNEUMONIA']  # Specify the class labels explicitly
@@ -41,7 +43,7 @@ def datagen_test_dir(dir, in_shape):
     return generator
 
 # Plot accuracy vs val_accuracy and Loss vs val_loss
-def plot_history(history, model_name):
+def plot_history(history: Any, model_name: str) -> Figure:
     plt.figure(figsize=(12, 4))
 
     # Plot training & validation accuracy values
@@ -65,7 +67,7 @@ def plot_history(history, model_name):
     plt.show()
     return plt.gcf()
 
-def plot_confusion_matrix(cm, model_name, classes=['NORMAL', 'PNEUMONIA']):
+def plot_confusion_matrix(cm: Any, model_name: str, classes: List[str] = ['NORMAL', 'PNEUMONIA']) -> Figure:
     plt.figure(figsize=(8, 6))
     sns.heatmap(cm, annot=True, fmt='d', cmap=plt.cm.Blues, xticklabels=classes, yticklabels=classes)
     plt.title(f'Confusion Matrix for {model_name}')
@@ -75,7 +77,7 @@ def plot_confusion_matrix(cm, model_name, classes=['NORMAL', 'PNEUMONIA']):
     return plt.gcf()
 
 # Calculate and plot ROC curve and AUC
-def plot_roc_auc(y_true, y_pred_proba, model_name):
+def plot_roc_auc(y_true: List[int], y_pred_proba: List[float], model_name: str) -> Figure:
     fpr, tpr, _ = roc_curve(y_true, y_pred_proba)
     roc_auc = auc(fpr, tpr)
 
